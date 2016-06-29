3451656b8e8d9bfb401aba3e2b84fdd7ab36b693
shlomta1 <Shlontz@gmail.com>
2016-06-29 18:07:40 +0300

    Commit & push of basic 7.1 ex

---
+++ CustomersApp/CustomersApp/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersApp
+{
+    public class Customer: IComparable<Customer>, IEquatable<Customer>
+    {
+        private int _mId ;
+        private string _mAddress;
+        private string _mName;
+
+        public int ID { get { return _mId; } }
+        public  string Name { get { return _mName; } }
+        public string Address { get { return _mAddress; } }
+
+        public Customer(int id, string name, string address)
+        {
+            this._mId = id;
+            this._mName = name;
+            this._mAddress = address;
+        }
+        public int CompareTo(Customer other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            return string.Compare(this.Name, other.Name, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public bool Equals(Customer other)
+        {
+            return other != null && this.Name.Equals(other.Name) && this.ID.Equals(other.ID);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID: {0}, Name: {1}, Address: {2}", ID, Name, Address);
+        }
+
+         
+    }
+}
---
+++ CustomersApp/CustomersApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Customer[] arr = new Customer[3];
+            arr[0] = new Customer(3, "shlomo", "address");
+            arr[1] = new Customer(99, "Haim", "address");
+            arr[2] = new Customer(21,"avi","address");
+
+            foreach (var customer in arr)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.WriteLine("After regular sorting....");
+            Array.Sort(arr);
+            foreach (var customer in arr)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.WriteLine("After AnotherCustomerComparer sorting");
+            Array.Sort(arr,new AnotherCustomerComparer());
+            foreach (var customer in arr)
+            {
+                Console.WriteLine(customer);
+            }
+            Console.ReadKey();
+        }
+    }
+}
