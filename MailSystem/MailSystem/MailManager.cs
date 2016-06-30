802c6367a006ea1c92c991647270e8b2aeb9ca78
shlomta1 <Shlontz@gmail.com>
2016-06-30 15:40:08 +0300

    Implemented the Mail-Events program for the whole steps and demands

---
+++ MailSystem/MailSystem/MailManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSystem
+{
+    public class MailManager
+    {
+        public event EventHandler<MailArrivedEventArgs> MailArrived;
+
+        protected virtual void OnOnMailArrived(MailArrivedEventArgs e)
+        {
+            if (MailArrived != null)
+            {
+                MailArrived(this, e);
+            }
+        }
+       
+        public void SimulateMailArrived(string title,string body)
+        {
+            OnOnMailArrived(new MailArrivedEventArgs(title,body));
+        }
+    }
+
+    public class MailArrivedEventArgs
+    {
+        private string _mTitle;
+        private string _mBody;
+
+        public MailArrivedEventArgs(string title, string body)
+        {
+            _mTitle = title;
+            _mBody = body;
+        }
+        public string Title { get { return _mTitle; } }
+        public string Body { get { return _mBody; } }
+    }
+}
---
+++ MailSystem/MailSystem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MailSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailManager mgr= new MailManager();
+            mgr.MailArrived += Mgr_MailArrived;
+            Timer timer = new Timer(EventsCallbck, mgr, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+
+            Console.ReadKey();
+        }
+
+        private static void Mgr_MailArrived(object sender, MailArrivedEventArgs e)
+        {
+            Console.WriteLine($"Message title: {e.Title}, Message body: {e.Body}");
+        }
+
+        private static void EventsCallbck(Object obj)
+        {
+            var mgr = obj as MailManager;
+            if (mgr != null)
+            {
+                mgr.SimulateMailArrived("Hi all", "This is the message body");
+            }
+        }
+    }
+}
