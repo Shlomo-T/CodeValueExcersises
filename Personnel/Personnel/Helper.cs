1898af1e4c3186be80d0f4e567e52331988a0e25
shlomta1 <Shlontz@gmail.com>
2016-07-02 13:59:39 +0300

    -Created the 'Names.txt' file -Create the method that read the 'Names.txt' file

---
+++ Personnel/Personnel/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    public class Helper
+    {
+        public string GetFilePath()
+        {
+            // File could be in every path that we want
+            string path = ConfigurationManager.AppSettings["FilePath"];
+            return path;
+        }
+        
+        public string[] ReadFromFile(StreamReader reader)
+        {
+            // if there's a line to read the reader will make sure to read it
+            List<string> comp = new List<string>();
+            while (reader.Peek() >= 0)
+            {
+                string name = reader.ReadLine();
+                if (!string.IsNullOrEmpty(name))
+                {
+                    comp.Add(name);
+                }
+            }
+            return comp.ToArray();
+        }
+    }
+}
---
+++ Personnel/Personnel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper helper= new Helper();
+            string path = helper.GetFilePath();
+            StreamReader reader= new StreamReader(path);
+            string[] Names = helper.ReadFromFile(reader);
+
+            foreach (var name in Names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
