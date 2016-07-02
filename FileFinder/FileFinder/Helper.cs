f09f03dcca4cbc7455abc484d820c25fa3f949f2
shlomta1 <Shlontz@gmail.com>
2016-07-02 14:59:27 +0300

    Create the Filefinder project and implemented the recursion as requested in ex 10.2

---
+++ FileFinder/FileFinder/Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace FileFinder
+{
+    public class Helper
+    {
+        public bool CheckArgs(string[] args)
+        {
+            if (args != null && args.Length >= 2 && !string.IsNullOrEmpty(args[0]) && !string.IsNullOrEmpty(args[1]))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void SearchForFilesrecursively(ICollection<DirectoryInfo> dirs, ICollection<FileInfo> files,
+            List<FileInfo> ans,string subString)
+        {
+            if ((files == null || !files.Any()) && (dirs == null || !dirs.Any()))
+            {
+                return;
+            }
+
+            if (files != null && files.Any())
+            {
+                foreach (var file in files)
+                {
+                    if (file != null && !string.IsNullOrEmpty(file.Name) && file.Name.Contains(subString))
+                    {
+                        ans.Add(file);
+                    }
+                }
+            }
+
+            if (dirs != null && dirs.Any())
+            {
+                foreach (var dir in dirs)
+                {
+                    if (dir!=null)
+                    {
+                        var fileList = dir.GetFiles("*.*");
+                        var dirList = dir.GetDirectories("*.*");
+                        SearchForFilesrecursively(dirList, fileList, ans,subString);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FileFinder/FileFinder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Helper helper= new Helper();
+            string dirPath=null, subString=null;
+            if (helper.CheckArgs(args))
+            {
+                dirPath = args[0];
+                subString = args[1];
+            }
+
+            if (!string.IsNullOrEmpty(dirPath) && !string.IsNullOrEmpty(subString))
+            {
+                DirectoryInfo dir = new DirectoryInfo(dirPath);
+                if (dir.Exists)
+                {
+                    //
+                    var fileList = dir.GetFiles("*.*");
+                    var dirList = dir.GetDirectories("*.*");
+                    List<FileInfo> ans = new List<FileInfo>();
+
+                    // startpoint of recursion
+                    helper.SearchForFilesrecursively(dirList, fileList, ans, subString);
+
+                    if (ans != null && ans.Any())
+                    {
+                        foreach (var file in ans)
+                        {
+                            Console.WriteLine($"File:{file} File Length:{file.Length}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unexpected parameters...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
