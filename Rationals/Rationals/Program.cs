51750bd928e40924654eed65c019ced42f8a7910
shlomta1 <Shlontz@gmail.com>
2016-07-02 15:03:40 +0300

    Add rationals project to repository

---
+++ Rationals/Rationals/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational first = new Rational(6,12);
+            Rational sec = new Rational(12, 6);
+            Rational third = new Rational(5);
+
+            Console.WriteLine("{0} object {1}","first", first);
+            Console.WriteLine("{0} object {1}", "sec", sec);
+            Console.WriteLine("{0} object {1}", "third", third);
+
+            Console.WriteLine("reducing all objects");
+            first.Reduce();
+            sec.Reduce();
+            third.Reduce();
+
+            Console.WriteLine("Adding...");
+            Console.WriteLine("Add first object and second object...");
+            Console.WriteLine("Result is {0}",first.Add(sec));
+            Console.WriteLine("Add second object and third object...");
+            Console.WriteLine("Result is {0}", sec.Add(third));
+
+            Console.WriteLine("Multiplying...");
+            Console.WriteLine("Multiplying first object and second object...");
+            Console.WriteLine("Result is {0}", first.Mul(sec));
+            Console.WriteLine("Multiplying second object and third object...");
+            Console.WriteLine("Result is {0}", sec.Mul(third));
+
+            if (new Rational(5, 5).Equals(new Rational(5, 5)) && !new Rational(5, 5).Equals(new Rational(5, 3)))
+            {
+                Console.WriteLine("Equals working perfect...");
+            }
+            try
+            {
+                Rational rationalWithException = new Rational(5,0);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Not valid Rational was caught by the test...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rationals/Rationals/Rational.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    public struct Rational
+    {
+        public int Numerator { get { return numerator; } }
+        public int Denominator { get { return denominator; } }
+        public double Value { get { return (double)numerator / denominator; } }
+
+        private int numerator;
+        private int denominator;
+
+        public Rational(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException();
+            }
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public Rational(int numerator)
+        {
+            this.numerator = numerator;
+            this.denominator = 1;
+        }
+
+        public Rational Add(Rational rt)
+        {
+            // Adding two rational object
+            this.Reduce();
+            rt.Reduce();
+            if (rt.denominator == this.denominator)
+            {
+                return new Rational(this.numerator + rt.numerator, this.denominator);
+
+            }
+
+            int numerator = this.numerator * rt.denominator + rt.numerator * this.denominator;
+            int denominator = this.denominator * rt.denominator;
+            return new Rational(numerator, denominator);
+        }
+        public Rational Mul(Rational rt)
+        {
+            int numerator = this.numerator * rt.numerator;
+            int denominator = this.denominator * rt.denominator;
+            return new Rational(numerator, denominator);
+        }
+
+        public void Reduce()
+        {
+            //using GCD algorithm to simplify the Rational object 
+            int gcd = Gcd(Math.Max(this.numerator, this.denominator), Math.Min(this.numerator, this.denominator));
+            if (gcd != 0)
+            {
+                this.numerator /= gcd;
+                this.denominator /= gcd;
+            }
+        }
+        private int Gcd(int a, int b)
+        {
+            //implementing  recursive GCD algorithm
+            return b == 0 ? a : Gcd(b, a % b);
+        }
+        public override string ToString()
+        {
+            return string.Format("Rational - Numerator:{0} denominator:{1} and value is {2}", this.numerator, this.denominator,this.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == typeof (Rational) && ((Rational) obj).numerator == this.numerator &&
+                   ((Rational) obj).denominator == this.denominator;
+            
+        }
+    }
+}
